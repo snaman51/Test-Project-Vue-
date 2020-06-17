a4971dc237599ed03c8a3d8f9cb5356a229b709b
Samson Naman <snaman51@live.com>
2020-06-17 15:38:23 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {db} from '@/db.js';
+export default {
+  name: 'app',
+  data () {
+     products:[],
+    Ele:'',
+    quanty:0,
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    firestore: {
+    products: db.collection('Test')
+    
+  },
+  computed: {
+    totalProducts () {
+      return this.products.reduce((sum,product) => {
+        return sum+Number(product.qty)},0)
+      
+    }
+  },
+  
+  methods: {
+    addEle:function() {
+  var name2=document.getElementById("name2").value;
+  var qty2=document.getElementById("qty2").value;
+
+
+      db.collection("Test").add({name:name2, qty:qty2})
+      .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
